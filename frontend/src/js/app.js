26d3e6f4b4137145fa7eef7f72937e9072f4a3cc
Dawid Zawrotny <zawrotny@protonmail.com>
2018-03-01 11:15:34 +0100

    Reorganize frontend structure

--- frontend/src/js/app.js
+++ frontend/src/js/app.js
@@ -1,1 +1,19 @@
-console.log('Hello world!');
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+import '../styles/style.css';
+
+import logoImage from '../assets/logo.svg';
+
+export default class Hello extends Component {
+    render() {
+        return (
+            <div>
+                Hello from react
+                <img src={logoImage} alt="logo" />
+            </div>
+        );
+    }
+}
+
+render(<Hello />, document.getElementById('app'));
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -3,54 +3,54 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 // Constant with your paths
 const paths = {
-// Put your structure paths here
-DIST: path.resolve(__dirname, 'dist'),
-SRC: path.resolve(__dirname, 'src'),
-JS: path.resolve(__dirname, 'src/js'),
-PUBLIC: path.resolve(__dirname, 'public'),
+  // Put your structure paths here
+  DIST: path.resolve(__dirname, 'dist'),
+  SRC: path.resolve(__dirname, 'src'),
+  JS: path.resolve(__dirname, 'src/js'),
+  PUBLIC: path.resolve(__dirname, 'public'),
 };
 // Webpack configuration
 // Change your entry point here
 module.exports = {
- entry: path.join(paths.JS, 'app.js'),
- output: {
- path: paths.DIST,
- filename: 'app.bundle.js',
-},
-// Tell webpack to use html plugin
-plugins: [
- 
- new HtmlWebpackPlugin({
-  template: path.join(paths.PUBLIC, 'index.html'),
- }),
- 
- new ExtractTextPlugin('style.bundle.css'), 
-],
-// Loaders configuration
-// We are telling webpack to use 'babel-loader' for .js and .jsx // files
-module: {
- rules: [
-  {
-   test: /\.(js|jsx)$/,
-   exclude: /node_modules/,
-   use: ['babel-loader'],
-  },{
-   test: /\.css$/,
-   loader: ExtractTextPlugin.extract({
-    use: 'css-loader',
-   }),
-  },{
-   test: /\.scss$/,
-   loader: ExtractTextPlugin.extract({ 
-    use: ['css-loader', 'sass-loader'],
-   }),
-  },{
-   test: /\.(png|jpg|gif)$/,
-   use: ['file-loader'],
+  entry: path.join(paths.JS, 'app.js'),
+  output: {
+    path: paths.DIST,
+    filename: 'app.bundle.js',
   },
-],
-},
-resolve: {
- extensions: ['.js', '.jsx'],
-},
+  // Tell webpack to use html plugin
+  plugins: [
+
+    new HtmlWebpackPlugin({
+      template: path.join(paths.PUBLIC, 'index.html'),
+    }),
+
+    new ExtractTextPlugin('style.bundle.css'),
+  ],
+  // Loaders configuration
+  // We are telling webpack to use 'babel-loader' for .js and .jsx // files
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      }, {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          use: 'css-loader',
+        }),
+      }, {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract({
+          use: ['css-loader', 'sass-loader'],
+        }),
+      }, {
+        test: /\.(png|jpg|gif|svg)$/,
+        use: ['file-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
 };
